0e65bedba966fa5dd90d250df1487ac1c59b8a73
renancassi <renanvic7@gmail.com>
2026-03-18 15:06:33 -0300

    feat(shorten): enhance URL shortening and retrieval functionality

--- api/src/routes/shorten.route.ts
+++ api/src/routes/shorten.route.ts
@@ -1,28 +1,37 @@
 import { FastifyInstance } from "fastify";
-import { prisma } from "../prisma/prisma";
-import { Prisma } from "@prisma/client";
 import { z } from "zod";
 
-
 export async function shortenRoute(app: FastifyInstance) {
-    const urlBodySchema = z.object({
+    const shortenBodySchema = z.object({
         url: z.string().url(),
     });
 
+    const shortenParamsSchema = z.object({
+        code: z.string(),
+    });
+
+    const responseSchema = z.object({
+        code: z.string(),
+    });
+
+    const errorSchema = z.object({
+        message: z.string(),
+    });
+
+    // POST /shorten
     app.post(
-        '/shorten',
+        "/shorten",
         {
             schema: {
-                body: urlBodySchema,
+                body: shortenBodySchema,
                 response: {
-                    201: z.object({ code: z.string() }),
-                    400: z.object({ message: z.string() }),
+                    201: responseSchema,
+                    400: errorSchema,
                 },
             },
         },
-        async (request, reply) => {
-            const { url } = request.body;
-
+        async (req, res) => {
+            const { url } = shortenBodySchema.parse(req.body);
             const code = Math.random().toString(36).substring(2, 8);
 
             try {
@@ -30,14 +39,57 @@ export async function shortenRoute(app: FastifyInstance) {
                     data: {
                         code,
                         originalUrl: url,
-                        qrCode: '',
+                        qrCode: "",
                     },
                 });
 
-                return reply.status(201).send({ code });
-            } catch (err) {
-                return reply.status(400).send({
-                    message: 'Error creating short URL',
+                return res.status(201).send({ code });
+            } catch {
+                return res.status(400).send({
+                    message: "Error creating short URL",
+                });
+            }
+        }
+    );
+
+    // GET /:code
+    app.get(
+        "/:code",
+        {
+            schema: {
+                params: shortenParamsSchema,
+                response: {
+                    404: errorSchema,
+                },
+            },
+        },
+        async (req, res) => {
+            const { code } = shortenParamsSchema.parse(req.params);
+
+            try {
+                const shortUrl = await app.prisma.short_urls.findUnique({
+                    where: { code },
+                });
+
+                if (!shortUrl) {
+                    return res.status(404).send({
+                        message: "Short URL not found",
+                    });
+                }
+
+                await app.prisma.short_urls.update({
+                    where: { code },
+                    data: {
+                        clicks: {
+                            increment: 1,
+                        },
+                    },
+                });
+
+                return res.redirect(shortUrl.originalUrl);
+            } catch {
+                return res.status(404).send({
+                    message: "Error getting short URL",
                 });
             }
         }
