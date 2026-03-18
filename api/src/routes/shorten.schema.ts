19605ef15f5206ae3fd1a5e5be17bcde017f7bce
renancassi <renanvic7@gmail.com>
2026-03-18 16:24:17 -0300

    feat(schema): add validation schemas for URL shortening

---
+++ api/src/routes/shorten.schema.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+export const shortenBodySchema = z.object({
+    url: z.string().url(),
+});
+
+export const shortenParamsSchema = z.object({
+    code: z.string(),
+});
+
+export const responseSchema = z.object({
+    code: z.string(),
+});
+
+export const errorSchema = z.object({
+    message: z.string(),
+});
+
+export const shareResponseSchema = z.object({
+    code: z.string(),
+    url: z.string().url(),
+    shortUrl: z.string().url(),
+    clicks: z.number(),
+});
