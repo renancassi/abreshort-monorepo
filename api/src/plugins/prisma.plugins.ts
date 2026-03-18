833ec860f49db7a078880a668424da12189d4025
renancassi <renanvic7@gmail.com>
2026-03-18 15:21:35 -0300

    refactor(prisma): integrate PrismaPg adapter for database connection

--- api/src/plugins/prisma.plugins.ts
+++ api/src/plugins/prisma.plugins.ts
@@ -2,15 +2,23 @@ import fp from 'fastify-plugin';
 import { FastifyInstance } from "fastify";
 import { PrismaClient } from "../../generated/prisma/client";
 
+import { PrismaPg } from '@prisma/adapter-pg';
+
 async function prismaPluginImpl(app: FastifyInstance) {
-    const prisma = new PrismaClient({
-        log: ["error", "warn"],
+    const adapter = new PrismaPg({
+        connectionString: process.env.DATABASE_URL,
     });
+
+    const prisma = new PrismaClient({
+        adapter,
+        log: ['error', 'warn'],
+    });
+
     await prisma.$connect();
 
-    app.decorate("prisma", prisma);
+    app.decorate('prisma', prisma);
 
-    app.addHook("onClose", async () => {
+    app.addHook('onClose', async () => {
         await prisma.$disconnect();
     });
 }
