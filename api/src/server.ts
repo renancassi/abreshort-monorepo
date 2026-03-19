76d6e18c0c0029fb3d6036bbe91b45b4019d3a18
Renan Cassi <48595316+renancassi@users.noreply.github.com>
2026-03-19 18:59:31 -0300

    fix(server): change origin from string to boolean

--- api/src/server.ts
+++ api/src/server.ts
@@ -18,7 +18,7 @@ app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(fastifyCors, {
-    origin: 'true',
+    origin: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 });
 
